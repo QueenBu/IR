87ca1d15efcfaa175a3d8e4fd2bc1504778fb6eb
Asib Kamalsada <asib1999@gmail.com>
2020-06-26 16:10:57 +0200

    changed sentiment method to calculate sentiment for multiple words at once

--- ArgumentRetrievalSystem/src/tools/SentimentValues.java
+++ ArgumentRetrievalSystem/src/tools/SentimentValues.java
@@ -5,11 +5,10 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.Random;
 
 public class SentimentValues {
 
-    static HashMap<String, Integer> wordlist = new HashMap<String, Integer>();
+    static HashMap<String, Integer> sentimentWordMap = new HashMap<String, Integer>();
 
     static {
         fillList();
@@ -18,46 +17,52 @@ public class SentimentValues {
     /**
      * read the emotional words into a list
      */
-    public static void fillList(){
-        String csvFile = Paths.get("ArgumentRetrievalSystem", "corpus_files", "sentimentwords.csv").toAbsolutePath().normalize().toString();
+    public static void fillList() {
+        String csvFile =
+                Paths.get("ArgumentRetrievalSystem", "corpus_files", "sentimentwords.csv").toAbsolutePath().normalize().toString();
         String line = "";
 
-        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+        try ( BufferedReader br = new BufferedReader(new FileReader(csvFile)) ) {
 
-            while ((line = br.readLine()) != null) {
-                String [] temp = line.split(";");
+            while ( (line = br.readLine()) != null ) {
+                String[] temp = line.split(";");
                 try {
                     int value = Integer.parseInt(temp[ 1 ]);
-                    wordlist.put(temp[0], value);
-                } catch (NumberFormatException ex) {
+                    sentimentWordMap.put(temp[ 0 ].replaceAll("\\*", ".*"), value);
+                } catch ( NumberFormatException ex ) {
                     //skip
                 }
             }
 
-        } catch (IOException e){
+        } catch ( IOException e ) {
             e.printStackTrace();
         }
     }
 
     /**
      * determine if a word is emotional
-     *
+     * <p>
      * searches for a RegEx match
      *
-     * @param term a String that needs emotional evaluation
+     * @param docTerm a String that needs emotional evaluation
      * @return 1 for an emotional word 0 otherwise
      */
-    public static int termSentiment(String term) {
-        for ( String word : wordlist.keySet() ) {
-            if ( term.matches(word.replaceAll("\\*", ".*")) ) {
-                return 1;
+    public static int termSentiment(String docTerm) {
+        // W+  =  at least one non-word character which mean all character not a-z,A-Z,0-9,_
+        final String[] docWords = docTerm.split("\\W+");
+        int sum = 0;
+        for ( String docWord : docWords ) {
+            for ( String sentimentWord : sentimentWordMap.keySet() ) {
+                if ( docWord.matches(sentimentWord) ) {
+                    sum++;
+                }
             }
         }
-        return 0;
+        return sum;
     }
 
     public static int termSentimentWithoutPatternMatching(String term) {
-        return wordlist.containsKey(term) ? 1 : 0;
+        return sentimentWordMap.containsKey(term) ? 1 : 0;
     }
 
 }
